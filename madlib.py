a35c407a96058fb65c4114b6cb4cb5956c12db9d
Slinkiecici <slinkiecici@gmail.com>
2021-01-28 11:21:03 +0200

    new madlib

---
+++ madlib.py
@@ -0,0 +1,10 @@
+def madlib1 ():
+    name = input("what is the name of your hero?: ")
+    weapon = input("Your hero's weapon of choice?: ")
+    location = input("What world does your hero come from?: ")
+    villian = input("Who or what is your hero fighting?: ")
+
+    print (name + " was the greatest hero of " + location + ". They had fought many a beast and enemy but their greatest battle of all was agaisnt " + villian +
+    ", the fiercest of all. " + name + " defeated this evil using only their " + weapon + ".")
+
+madlib1()

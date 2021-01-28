d54882b9b1e236f3f58b6f5e41c144ef81fca266
Slinkiecici <slinkiecici@gmail.com>
2021-01-28 11:48:27 +0200

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -3,6 +3,7 @@ from random import randint
 def guess_the_number ():
     number = randint(0, 10)
     guessed = 1
+    score = 100
     while guessed == 1:
         print (number)
         guess = input("guess the number: ")
@@ -10,11 +11,14 @@ def guess_the_number ():
         if guess == number:
             print ("Well done!")
             guessed = 0
+            print ("You socred " + str(score) + "/100")
             break
         elif guess > number:
             print ("Your guess is too high!")
+            score = (score-1)
         elif guess < number:
             print ("Your guess is too low!")
+            score = (score-1)
         else:
             print ("Invalid input")
         continue
